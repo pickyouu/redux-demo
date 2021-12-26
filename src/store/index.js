05b3a9a646afc8c66600c15106b88f14e43905ee
rootzql <46565508+rootzql@users.noreply.github.com>
2021-12-26 13:53:29 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore,applyMiddleware} from "redux"
 import {reducer} from "../reducer/index"
 import thunk from "redux-thunk"
-const store= createStore(reducer,applyMiddleware(thunk))
+const store= createStore(reducer,applyMiddleware(thunk))//applyMiddleware(thunk) 一般store只能接收action对象;这个函数调用之后store就能接收函数，并执行
 export default store
