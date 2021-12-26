78c7109771218b1859cc856839ab8ef798b710bb
rootzql <2391714413@qq.com>
2021-12-26 22:36:43 +0800

    redux-devtools-extensions

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,9 +2,13 @@ import {createStore,applyMiddleware,combineReducers} from "redux"
 import  {count}  from "../reducer/count"
 import {person} from "../reducer/person"
 import thunk from "redux-thunk"
+import {composeWithDevTools} from "redux-devtools-extension"
 const allReducers=combineReducers({
   count,
   persons:person
 }) 
-const store= createStore(allReducers,applyMiddleware(thunk))//applyMiddleware(thunk) 一般store只能接收action对象;这个函数调用之后store就能接收函数，并执行
+const store= createStore(allReducers,composeWithDevTools(applyMiddleware(thunk)))
+//const store= createStore(allReducers,composeWithDevTools())
+//applyMiddleware(thunk) 一般store只能接收action对象;这个函数调用之后store就能接收函数，并执行
+
 export default store
